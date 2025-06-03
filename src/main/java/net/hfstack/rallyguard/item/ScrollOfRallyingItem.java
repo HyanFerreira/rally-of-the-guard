0a0170c0e5a16e4cbe0f47475bc9253dab659381
Hyan Ferreira <hcgamesmg2016@gmail.com>
2025-06-03 14:59:43 -0300

    Aplicação de um delay de 3s ao usar o item "Scroll Rallying" e mudança nos tipos de Guardas que serão teleportados para o jogador (Agora, apenas Guardas contratados serão teleportados)

--- src/main/java/net/hfstack/rallyguard/item/ScrollOfRallyingItem.java
+++ src/main/java/net/hfstack/rallyguard/item/ScrollOfRallyingItem.java
@@ -1,20 +1,20 @@
 package net.hfstack.rallyguard.item;
 
+import java.util.List;
+import java.util.Locale;
+
 import net.hfstack.rallyguard.effect.ModEffects;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
-
-import java.util.List;
-import java.util.Locale;
 
 public class ScrollOfRallyingItem extends Item {
 
@@ -28,6 +28,12 @@ public class ScrollOfRallyingItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!world.isClient) {
             if (user.isSneaking()) { // segurando SHIFT
+                // Verifica cooldown
+                if (user.getItemCooldownManager().isCoolingDown(this)) {
+                    user.sendMessage(Text.literal("Aguarde antes de usar novamente!"), true);
+                    return TypedActionResult.fail(user.getStackInHand(hand));
+                }
+
                 boolean hasRallyCommander = user.hasStatusEffect(ModEffects.RALLY_COMMANDER);
 
                 if (!(user instanceof ServerPlayerEntity serverPlayer)) {
@@ -43,7 +49,7 @@ public class ScrollOfRallyingItem extends Item {
                     // Executa comando para remover uma ação
                     try {
                         source.getServer().getCommandManager().executeWithPrefix(source,
-                                "execute as @e[type=guardvillagers:guard,distance=..100] at @s run data merge entity @s {Patrolling:0b,Following:0b}");
+                                "execute as @e[type=guardvillagers:guard,tag=contratado,distance=..100] at @s run data merge entity @s {Patrolling:0b,Following:0b}");
                     } catch (Exception e) {
                         user.sendMessage(Text.literal("Erro ao executar comandos para remover ação."), false);
                         e.printStackTrace();
@@ -57,17 +63,20 @@ public class ScrollOfRallyingItem extends Item {
                     // Executa comando para aplicar uma ação
                     try {
                         String spreadCommand = String.format(Locale.US,
-                                "spreadplayers %.1f %.1f 3 5 false @e[type=guardvillagers:guard,distance=..50]",
+                                "spreadplayers %.1f %.1f 3 5 false @e[type=guardvillagers:guard,tag=contratado,distance=..50]",
                                 user.getX(), user.getZ()
                         );
                         source.getServer().getCommandManager().executeWithPrefix(source, spreadCommand);
                         source.getServer().getCommandManager().executeWithPrefix(source,
-                                "execute as @e[type=guardvillagers:guard,distance=..50] at @s run data merge entity @s {Patrolling:0b,Following:1b}");
+                                "execute as @e[type=guardvillagers:guard,tag=contratado,distance=..50] at @s run data merge entity @s {Patrolling:0b,Following:1b}");
                     } catch (Exception e) {
                         user.sendMessage(Text.literal("Erro ao executar comandos para aplicar ação."), false);
                         e.printStackTrace();
                     }
                 }
+
+                // no final da ação aplica cooldown
+                user.getItemCooldownManager().set(this, 60); // 60 ticks = 3s
 
                 return TypedActionResult.success(user.getStackInHand(hand), false);
             } else {
