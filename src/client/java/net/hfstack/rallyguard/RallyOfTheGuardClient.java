5f4a19ccc4a885e527ee4a1397f188a3a9c655af
Hyan Ferreira <hcgamesmg2016@gmail.com>
2025-06-03 14:58:47 -0300

    Registrar a nova funcionalidade no lado do Cliente

--- src/client/java/net/hfstack/rallyguard/RallyOfTheGuardClient.java
+++ src/client/java/net/hfstack/rallyguard/RallyOfTheGuardClient.java
@@ -1,10 +1,11 @@
 package net.hfstack.rallyguard;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.hfstack.rallyguard.event.EmeraldContractHandler;
 
 public class RallyOfTheGuardClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
+		EmeraldContractHandler.register();
 	}
 }
